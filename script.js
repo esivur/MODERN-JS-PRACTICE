46b9c755ef301111129820504f2b04f47f78b2b5
andrew nouanthanuvanh <andrewnouanthanuvanh@MacBook-Pro-3.local>
2020-12-02 20:43:18 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var budgetController = (function(){
+
+    var x = 23;
+
+    var add = function(a){
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            console.log(add(b));
+        }
+    }
+})();
+
+var UIController = (function(){
+
+    //some code
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
